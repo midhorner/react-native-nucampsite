d0db427c12ad4643361be3c50994605dc4dcc421
midhorner <horner.andrew.e@gmail.com>
2021-05-29 11:36:22 -0400

    workshop 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -38,6 +38,7 @@ function RenderCampsite({
   const view = React.createRef();
 
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
+  const recognizeComment = ({ dx }) => (dx > 200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
@@ -72,6 +73,8 @@ function RenderCampsite({
           ],
           { cancelable: false },
         );
+      } else if (recognizeComment(gestureState)) {
+        onShowModal();
       }
       return true;
     },
